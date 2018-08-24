689cb2e5b12b615604b4f065ad4cb1b32a7381ad
unknown <kakazoo301@gmail.com>
2018-08-24 18:02:22 +0200

    explain why JSX

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-        <div>
-          <h1 className="App-title">Hello world</h1>
-        </div>
+        <h1>
+          Hello world
+        </h1>
     );
   }
 }
+
+//transferされてJSXからJSの形になる
+//JSXの方がhtmlタグをそのまま使用するのでわかりやすい
+// class App extends Component {
+//     render() {
+//         return React.createElement(
+//             "div",
+//             null,
+//             "Hello world"
+//         )
+//     }
+// }
 
 export default App;
