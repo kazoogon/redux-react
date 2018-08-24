5b1320ca22910b279ea866853cf50e7e21d70c35
kakaz <kakazoo209@gmail.com>
2018-08-24 18:44:27 +0200

    create class componet and functional component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 //ここのReactはJSX->JSに変換で使用する
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-        <React.Fragment>{/*divとかだしてくねーよ！　って時はこれ使って*/}
-            <label htmlFor="bar">bar</label>{/*ラベルのfor属性はこういう書き方*/}
-            <input type="text" onChange={() => {console.log("I am clicked")}} />
-        </React.Fragment>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//         <React.Fragment>{/*divとかだしてくねーよ！　って時はこれ使って*/}
+//             <label htmlFor="bar">bar</label>{/*ラベルのfor属性はこういう書き方*/}
+//             <input type="text" onChange={() => {console.log("I am clicked")}} />
+//         </React.Fragment>
+//     );
+//   }
 }
 
-//transferされてJSXからJSの形になる
-//JSXの方がhtmlタグをそのまま使用するのでわかりやすい
-// class App extends Component {
-//     render() {
-//         return React.createElement(
-//             "div",
-//             null,
-//             "Hello world"
-//         )
-//     }
-// }
+const App = () => {
+    return <div>Hi!!</div>
+}
 
 export default App;
