8369cca29db101812081dcd30b861c1c3ac979f4
kakaz <kakazoo209@gmail.com>
2018-08-24 21:08:15 +0200

    implement Store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; //作成したstoreの値を全componentから受け取れるようにできる
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'; //store作成のために必要なreducer
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>{/* Providerのおかげで、どのcomponentからもstoreの値が見れる */}
+    <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
