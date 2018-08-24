ced9ec6d12078d3f841ecb96ee0ebc2f77b12c2d
kakaz <kakazoo209@gmail.com>
2018-08-24 19:29:19 +0200

    about propTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //ここのReactはJSX->JSに変換で使用する
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // class App extends Component {
 //   render() {
@@ -16,7 +17,7 @@ const App = () => {
     const profiles = [
         {name: "sasaki", age: 10},
         {name: "masaki", age: 25},
-        {name: "who??"},
+        {name: 1},
     ]
     return (
       <div>
@@ -31,6 +32,11 @@ const App = () => {
 
 const User = (props) => {
     return <div>Hi, {props.name}!, and {props.age} yrs old</div>
+}
+
+User.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired
 }
 
 User.defaultProps = {
