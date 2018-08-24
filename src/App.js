cb92910c670d1ff8e0b5cdbf8a5343d199eaa842
kakaz <kakazoo209@gmail.com>
2018-08-24 19:53:47 +0200

    about state (setStateするとcall back関数でrender呼ばれます)

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
 //ここのReactはJSX->JSに変換で使用する
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-// class App extends Component {
-//   render() {
-//     return (
-//         <React.Fragment>{/*divとかだしてくねーよ！　って時はこれ使って*/}
-//             <label htmlFor="bar">bar</label>{/*ラベルのfor属性はこういう書き方*/}
-//             <input type="text" onChange={() => {console.log("I am clicked")}} />
-//         </React.Fragment>
-//     );
-//   }
-// }
 
-const App = () => {
-    const profiles = [
-        {name: "sasaki", age: 10},
-        {name: "masaki", age: 25},
-        {name: 1},
-    ]
-    return (
-      <div>
-          {
-              profiles.map((profile, index) => {
-                return <User name={profile.name} age={profile.age} key={index}/>
-              })
-          }
-      </div>
-    );
-}
+const App = () => (<Counter></Counter>)
 
-const User = (props) => {
-    return <div>Hi, {props.name}!, and {props.age} yrs old</div>
-}
+class Counter extends Component{
+    constructor(props){
+        super(props);{/* 親クラスで初期化 */}
+        this.state = { count: 0 }
+    }
 
-User.propTypes = {
-    name: PropTypes.string,
-    age: PropTypes.number.isRequired
-}
+    handlePlusButton = () => {
+        this.setState({count: this.state.count + 1});
+        {/* setStateはcall back関数でrenderが呼ばれる */}
+    }
 
-User.defaultProps = {
-    age: 1
+    render(){
+        return (
+            <React.Fragment>
+                counter { this.state.count }
+                <button onClick={this.handlePlusButton}>+1</button>
+            </React.Fragment>
+        )
+    }
 }
 
 export default App;
