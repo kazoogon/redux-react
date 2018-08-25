85c06701550ff5d754c8408476a914d64d41c0eb
kakaz <kakazoo209@gmail.com>
2018-08-25 13:04:42 +0200

    apply material-ui to events_new and events_show components

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'; //reduxのstoreからcomponentに値をもらえるようになるconnect関数
 import { Link } from 'react-router-dom'
 import { Field, reduxForm } from 'redux-form'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 import { postEvent } from "../actions"; //必要な関数をactionからimport
 
@@ -18,8 +20,14 @@ class EventsNew extends Component{
 
         return(
             <div>
-                <input {...input} placeholder={label} type={type} />
-                {touched && error && <span>{error}</span>}
+                <TextField
+                    hintText={label}
+                    floatingLabelText={label}
+                    type={type}
+                    errorText={touched && error}
+                    {...input}
+                    fullWidth={true}
+                />
             </div>
         )
     }
@@ -35,15 +43,14 @@ class EventsNew extends Component{
          ・2重押し回避に使う　submitting (submitしたらtrueになる)
         */
         const { handleSubmit, pristine, submitting, invalid} = this.props
+        const style = { margin: 12 }
         return (
             <form onSubmit={handleSubmit(this.onSubmit)}>
                 <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
                 <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
-                <div>
-                    <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-                    <Link to="/">Cancel</Link>
-                </div>
 
+                <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+                <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />}/>
             </form>
         )
     }
--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 import { Field, reduxForm } from 'redux-form'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 import { getEvent, deleteEvent, putEvent } from "../actions"; //必要な関数をactionからimport
 
@@ -20,11 +22,18 @@ class EventsShow extends Component{
     renderField(field){
         const { input, label, type, meta: { touched, error }} = field
         //redux特有のmeta属性 "touched" (一回でも触れたら)
+        const style = { margin: 12 }
 
         return(
             <div>
-                <input {...input} placeholder={label} type={type} />
-                {touched && error && <span>{error}</span>}
+                <TextField
+                    hintText={label}
+                    floatingLabelText={label}
+                    type={type}
+                    errorText={touched && error}
+                    {...input}
+                    fullWidth={true}
+                />
             </div>
         )
     }
@@ -46,16 +55,18 @@ class EventsShow extends Component{
          ・2重押し回避に使う　submitting (submitしたらtrueになる)
         */
         const { handleSubmit, pristine, submitting, invalid} = this.props
+        const style = { margin: 12 }
+        
         return (
             <form onSubmit={handleSubmit(this.onSubmit)}>
                 <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
                 <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
-                <div>
-                    <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-                    <Link to="/">Cancel</Link>
-                    <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
-                </div>
 
+                <div>
+                    <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+                    <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />}/>
+                    <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick}/>
+                </div>
             </form>
         )
     }
